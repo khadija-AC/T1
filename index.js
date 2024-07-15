67f7bf4978a92e126cc301f4b24f07189d4aac40
khadija-AC <khadijaaws21@gmail.com>
2024-07-15 17:38:54 +0100

    feat(update): update pour teste

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-(function (exports) {
+;(function (exports) {
   function greet(name) {
     const options = {
+      hour: 'numeric',
+
       day: 'numeric',
       month: 'long',
-      year: 'numeric'
-      weekday: 'numeric'
+      year: 'numeric',
+      weekday: 'numeric',
     }
 
     const now = new Date()
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
-  };
+  }
 
   exports.greet = greet
 })(this)
