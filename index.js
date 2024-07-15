9eae75c4a684301c62c7b161d248b02043b275ec
khadija-AC <khadijaaws21@gmail.com>
2024-07-15 13:52:30 +0100

    pleaaz sd9i

---
+++ index.js
@@ -0,0 +1,26 @@
+(function (exports) {
+  
+    
+
+
+
+
+
+
+  
+  function greet(name) {
+    
+
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString("en-US", options);
+    return `Hello, ${name}! Today is ${formattedDate}.`;
+  }
+
+  exports.greet = greet;
+})(this);
