982365026397989ce1d42b55f94bd5cf74a1330a
khadija-AC <khadijaaws21@gmail.com>
2024-07-15 17:28:49 +0100

    fix(erreur): fix error

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-;(function (exports) {
+(function (exports) {
   function greet(name) {
     const options = {
       day: 'numeric',
       month: 'long',
-      year: 'numeric',
-      weekday: 'numeric',
+      year: 'numeric'
+      weekday: 'numeric'
     }
 
     const now = new Date()
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
-  }
+  };
 
   exports.greet = greet
 })(this)
