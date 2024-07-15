2c41d7e30894a20aa1f4d905a62839da6061c505
khadija-AC <khadijaaws21@gmail.com>
2024-07-15 20:15:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 (function (exports) {
   function greet(name) {
     const options = {
-
-
-
       hour: "numeric",
-
-
-
-
-
-
+      day: 'numeric',
       month: 'long',
       year: 'numeric',
       weekday: 'numeric',
@@ -20,6 +12,3 @@
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
   };
-
-  exports.greet = greet
-})(this)
