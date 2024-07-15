ddf20bf5535bfc8a6668ba1aa96b870e8f6359d5
khadija-AC <khadijaaws21@gmail.com>
2024-07-15 16:59:38 +0100

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-(function (exports) {
+;(function (exports) {
   function greet(name) {
     const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    const now = new Date();
-    const formattedDate = now.toLocaleDateString("en-US", options);
-    return `Hello, ${name}! Today is ${formattedDate}.`;
+      day: 'numeric',
+    }
+    const now = new Date()
+    const formattedDate = now.toLocaleDateString('en-US', options)
+    return `Hello, ${name}! Today is ${formattedDate}.`
   }
 
-  exports.greet = greet;
-})(this);
+  exports.greet = greet
+})(this)
